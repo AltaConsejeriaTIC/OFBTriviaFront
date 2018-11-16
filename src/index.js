2c2d0bd43fe68be8dd2b767bc46c65db1a139dfd
Alejandro Diaz <aldiazve@gmail.com>
2018-11-16 12:59:39 -0500

    set up

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// DO NOT MODIFY THE CONTENT NOR THE NAME OF THIS FILE.
+// ORQUESTA FILARMÓNICA DE BOGOTA - TRIVIA ADMIN CLIENT
+// index.js - Main entry point
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+import IronHacksApp from './ironhackApp.js';
+
+import { BrowserRouter } from 'react-router-dom' // Router
+
+//Main CSS
+import './main.css';
+
+ReactDOM.render((
+  <BrowserRouter>
+    <OFBTriviaApp/>
+  </BrowserRouter>
+), document.getElementById('root'));
+
+registerServiceWorker();
+
+// DO NOT MODIFY THE CONTENT NOR THE NAME OF THIS FILE.
